0610f1141fdd0dca3ea57b88c01330e5a7fe49d7
libowen <1064416560@qq.com>
2019-04-13 19:58:53 +0800

    :zap: feat: 增加了投资者信息明细的参数

--- src/pages/Investor/InvestorInfoManagement.js
+++ src/pages/Investor/InvestorInfoManagement.js
@@ -1,17 +1,23 @@
 import React, { PureComponent } from 'react'
 import {
-  Form, Input, Button, Card, Avatar
+  Form, Input, Button, Card,
 } from 'antd';
 import { connect } from 'dva'
 // import styles from './InvestorManagement.less'
-
-const fakeAvatar = "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1554379058&di=04c565d8258a79cba4e827425e60f6bc&src=http://img.zcool.cn/community/01a3865ab91314a8012062e3c38ff6.png@1280w_1l_2o_100sh.png"
 
 @connect()
 class InvestorInfoManagement extends PureComponent {
   constructor(props){
     super(props)
     this.state = {
+      user: {
+        loginName: 'investor',
+        password: 'skyorange',
+        realName: 'skyorange',
+        email: '12345678@email.com',
+        phone: '12345678901',
+        lastLoginTime: '2019-4-13'
+      }
     }
   }
 
@@ -23,38 +29,45 @@ class InvestorInfoManagement extends PureComponent {
     const buttonItemLayout ={
       wrapperCol: { span: 14, offset: 4 },
     };
+    const { user } = this.state;
     return (
       <Card bordered={false}>
         <Form layout="horizontal">
           <Form.Item
-            label="头像"
+            label="登录名"
             {...formItemLayout}
           >
-            <Avatar style={{width:100, height: 100}} src={fakeAvatar} />
+            <Input value={user.loginName} disabled  />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            {...formItemLayout}
+          >
+            <Input value={user.password} />
           </Form.Item>
           <Form.Item
             label="姓名"
             {...formItemLayout}
           >
-            <Input placeholder="此处登录后展示原姓名" />
+            <Input value={user.realName} disabled  />
           </Form.Item>
           <Form.Item
-            label="性别"
+            label="邮箱"
             {...formItemLayout}
           >
-            <Input placeholder="此处登录后展示原性别" />
+            <Input value={user.email} />
           </Form.Item>
           <Form.Item
-            label="年龄"
+            label="手机"
             {...formItemLayout}
           >
-            <Input placeholder="此处登录后展示原姓名" />
+            <Input value={user.phone} />
           </Form.Item>
           <Form.Item
-            label="资本"
+            label="最后一次登录时间"
             {...formItemLayout}
           >
-            <Input placeholder="此处登录后展示原资本" />
+            <span>{user.lastLoginTime}</span>
           </Form.Item>
           <Form.Item {...buttonItemLayout}>
             <Button type="primary">提交修改</Button>
