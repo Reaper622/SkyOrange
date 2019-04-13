3f8d33e207648dd6bf443b4072cb735f5478f8fd
libowen <1064416560@qq.com>
2019-04-13 21:17:19 +0800

    :zap: feat: 增加了提款申请的方式

--- src/pages/Investor/InvestorManagement.js
+++ src/pages/Investor/InvestorManagement.js
@@ -10,7 +10,17 @@ class InvestorManagement extends PureComponent {
   constructor(props){
     super(props)
     this.state = {
+      benchmark: '',
+      current: '',
+      net: '',
+      margin: ''
     }
+  }
+
+  changeState = (value, index) => {
+    this.setState({
+      [index]: value
+    })
   }
 
   render() {
@@ -21,20 +31,34 @@ class InvestorManagement extends PureComponent {
     const buttonItemLayout ={
       wrapperCol: { span: 14, offset: 4 },
     };
+
+    const application = this.state;
     return (
       <Card bordered={false}>
         <Form layout="horizontal">
           <Form.Item
-            label="提款目的"
+            label="基准值"
             {...formItemLayout}
           >
-            <Input placeholder="输入提款目的" />
+            <Input placeholder="输入提款基准值" value={application.benchmark} onChange={e => this.changeState(e.target.value, 'benchmark')}  />
           </Form.Item>
           <Form.Item
-            label="提款金额"
+            label="现值"
             {...formItemLayout}
           >
-            <Input placeholder="输入提款金额" />
+            <Input placeholder="输入提款现值" value={application.current} onChange={e => this.changeState(e.target.value, 'current')} />
+          </Form.Item>
+          <Form.Item
+            label="净值"
+            {...formItemLayout}
+          >
+            <Input placeholder="输入提款净值" value={application.net} onChange={e => this.changeState(e.target.value, 'net')} />
+          </Form.Item>
+          <Form.Item
+            label="保证金"
+            {...formItemLayout}
+          >
+            <Input placeholder="输入提款保证金" value={application.margin} onChange={e => this.changeState(e.target.value, 'margin')} />
           </Form.Item>
           <Form.Item {...buttonItemLayout}>
             <Button type="primary">提交申请</Button>
