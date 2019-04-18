7b0acdf6c760bdd9f9b4c685e6f04d3c79fbfb1e
libowen <1064416560@qq.com>
2019-04-18 16:52:22 +0800

    :zap: feat: 修改了管理者决策的展示方式

--- src/pages/TradeDecision/AdminDecision.js
+++ src/pages/TradeDecision/AdminDecision.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Yuan from '@/utils/Yuan'
 import { connect } from 'dva'
-import { Table, Divider } from 'antd'
+import { Table, Divider, Row, Col, Card, Button } from 'antd'
 import styles from './AdminDecision.less'
 
 @connect(({ decision, loading}) => ({
@@ -75,7 +75,37 @@ class AdminDecision extends Component {
     const { decision } = this.props;
     return (
       <div>
-        <Table columns={columns} dataSource={decision.decisions} style={{background: '#fff', marginTop: 50}} />
+        {/* <Table columns={columns} dataSource={decision.decisions} style={{background: '#fff', marginTop: 50}} /> */}
+        <Row type="flex" justify="start" gutter={8}>
+          {decision.decisions.map(item => (
+            <Col xs={20} sm={16} md={12} lg={10} xl={6} style={{marginBottom: 10}}>
+              <Card
+                title={item.solutionName}
+                extra={item.solutionID}
+              >
+                <Row style={{marginBottom: 30}}>
+                  <Col span={8}>
+                    <Yuan>{item.tradeMoney}</Yuan>
+                  </Col>
+                  <Col span={8}>
+                    <span>{item.tradePerson}</span>
+                  </Col>
+                  <Col span={8}>
+                    <span>{item.status === 1? '通过' : '待评定'}</span>
+                  </Col>
+                </Row>
+                <Row type="flex" justify="space-around">
+                  <Col span={8}>
+                    <Button type="primary" block>通过</Button>
+                  </Col>
+                  <Col span={8}>
+                    <Button type="danger" block>拒绝</Button>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
     )
   }
